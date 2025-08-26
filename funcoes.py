ab765936dfd6268e6e320598138398a274fa354a
nicklaranjeira <nicolle.laranjeira02@gmail.com>
2025-08-26 08:26:30 -0300

    esqueleto do software

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,23 @@
+livros = {
+        'ROMANCE': {},
+        'TERROR' : {},
+        'FANTASIA' : {}
+}
 
+def cadastro_user():
+    pass
+
+def cadastro_livro():
+    pass
+
+def emprestar():
+    pass
+
+def devolver():
+    pass
+
+def atualizar():
+    pass 
+
+def listar_livros():
+    pass
