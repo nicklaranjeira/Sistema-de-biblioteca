1501d7f891d6b5eba6e80dd4cdfcf7fd1c34b06d
jhonbslv <jhonbslv@gmail.com>
2025-08-26 08:54:35 -0300

    cadastro livro

--- funcoes.py
+++ funcoes.py
@@ -8,6 +8,13 @@ def cadastro_user():
     pass
 
 def cadastro_livro():
+    titulo = input("Digite o titulo do livro: ")
+    categorialivros = input("Digite a categoria do livro (ROMANCE TERROR FANTASIA): ").upper()
+    if categorialivros in livros:
+        livros[categorialivros][titulo] = {}
+        print(f"Livro '{titulo}' cadastrado com sucesso na categoria {categorialivros}.")
+    else:
+        print("Categoria errada")
     pass
 
 def emprestar():
